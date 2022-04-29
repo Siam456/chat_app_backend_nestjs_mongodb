230ed58a5b590ba848ee740b4dffa62681fc58ec
Siam456 <hasibul15-11038@diu.edu.bd>
2022-04-29 23:31:34 +0600

    feat: retrive all user from db

--- src/user-module/controller/user.controller.ts
+++ src/user-module/controller/user.controller.ts
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
+import { User } from '../interface/user.interface';
 import { UserService } from '../service/user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('test/hello')
   getHello(): string {
     return this.userService.getHello();
+  }
+
+  @Get()
+  async getAllUser(): Promise<User[]> {
+    return this.userService.getAllUsers();
   }
 }
--- src/user-module/service/user.service.ts
+++ src/user-module/service/user.service.ts
@@ -1,9 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../interface/user.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel('user') private readonly userModel: Model<User>) {}
   getHello(): string {
     return 'Hello World! from user';
+  }
+  //retrive all user
+  async getAllUsers(): Promise<User[]> {
+    const response = await this.userModel.find({});
+    if (response) {
+      return Promise.resolve(response);
+    }
+    throw new NotFoundException('data not found');
   }
 }
